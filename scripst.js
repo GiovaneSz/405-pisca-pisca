831c51644bbdf050312c8f0ce394626e1aedf4df
giovane <giovanesantlima@gmail.com>
2019-07-25 17:40:32 -0300

    adicionar troca de cores

---
+++ scripst.js
@@ -0,0 +1,21 @@
+// let botao = document.querySelector("button")
+// let segundos = prompt("qual a velocidade por segundo vc quer?");
+// let milisegundos = segundos * 1000;
+// setTimeout(()=>{
+//     console.log("Giovane");
+// },5000);
+
+// setInterval(() => {
+//     console.log("Oi Giovane")
+// },2000);
+
+// let cronometro = setInterval(() => {
+//     console.log("Giovane")
+// },500);
+
+// function pararTempo(){
+//     clearInterval(cronometro);
+// }
+
+// botao.onclick = pararTempo
+
